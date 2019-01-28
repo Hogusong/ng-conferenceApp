b56d723c9a103141313d4bed860a4f9872d2887b
“Young <hogusong1@gmail.com>
2019-01-27 21:03:36 -0500

    updated 'user service' to handle user's databases from firebase store and storage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,20 @@ import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { MapComponent } from './pages/map/map.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TutorialComponent } from './pages/tutorial/tutorial.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SupportComponent } from './pages/support/support.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 const routes: Routes = [
   { path: 'schedule', component: ScheduleComponent },
   { path: 'speakers', component: SpeakersComponent },
   { path: 'map', component: MapComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'support', component: SupportComponent },
+  { path: 'signin', component: SigninComponent },
   { path: 'tutorial', component: TutorialComponent},
   { path: '', redirectTo: 'tutorial', pathMatch: 'full' },
 ];
--- src/app/providers/user.service.ts
+++ src/app/providers/user.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { USER } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  usersCollection: AngularFirestoreCollection<USER>;
+  userDoc: AngularFirestoreDocument<USER>;
+
+  constructor(private db: AngularFirestore,
+              private fireStorage: AngularFireStorage) {
+    this.usersCollection = this.db.collection(
+      'users', ref => ref.orderBy('username', 'asc'));
+  }
+
+  getUsers(): Observable<USER[]> {
+    return this.usersCollection.snapshotChanges()
+      .pipe(map(response => {
+        return response.map(action => {
+          const data = action.payload.doc.data() as USER;
+          data.id = action.payload.doc.id;
+          return data;
+        });
+      }));
+  }
+
+  getUserById(id: string): Promise<USER> {
+    return this.usersCollection.doc(id).ref.get()
+      .then(doc => {
+        const user = doc.data() as USER;
+        user.id = id;
+        if (user.avatar) {
+          // download image from firebase storage.
+          this.fireStorage.ref(user.avatar).getDownloadURL().subscribe(url => {
+            user.avatar = url;
+          });
+        }
+      return user;
+    });
+  }
+
+  // return result including user's ID.
+  addUser(user: USER): Promise<any> {
+    return this.usersCollection.add(user);
+  }
+
+  updateUser(user: USER) {
+    const id = user.id;
+    delete(user.id);
+    this.userDoc = this.db.doc(`users/${id}`);
+    this.userDoc.update(user)
+    user.id = id;
+  }
+
+  removeUser(user: USER) {
+    this.userDoc = this.db.doc(`users/${user.id}`);
+    this.userDoc.delete();
+  }
+
+  deleteUrl(oldUrl) {
+    if (oldUrl) {
+      this.fireStorage.storage.refFromURL(oldUrl).delete();
+    }
+  }
 }
