b0ee402870831338ecef1a4f384a2aa155d14225
“Young <hogusong1@gmail.com>
2019-01-29 01:00:39 -0500

    updated 'home' page and changed images of 'tutorial' page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeneralService } from './providers/general.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  
+  loggedIn: boolean;
+
+  constructor(private genService: GeneralService,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.genService.isLoggedIn().then(res => {
+      if (res) {
+        this.loggedIn = res;
+        this.router.navigate(['schedule']);
+      } else {
+        this.router.navigate(['home']);
+      }
+    })    
+  }
+}
